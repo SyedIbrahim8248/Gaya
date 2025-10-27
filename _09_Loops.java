a2056e62b03b001bc727b9bb6d7b190e147ca026
Syed Ibrahim <187879720+SyedIbrahim8248@users.noreply.github.com>
2025-10-27 23:54:39 +0530

    Add files via upload

---
+++ _09_Loops.java
@@ -0,0 +1,31 @@
+package Javabymanvimaampw.java.java;
+
+public class _09_Loops {
+    public static void main(String[] args) {
+        System.out.println(1);
+        System.out.println(2);
+        System.out.println(3);
+       //1- 100
+       //for(int i = 1; i <= 100; i++){
+           // System.out.println(i);
+       // 100 = 1
+        //for(int j = 100; j >=1; j = j-1){
+            //System.out.println(j);
+        //while loops
+       // int i = 100;
+       // while(i >= 1){
+           // System.out.println(i);
+            //i = i
+       // do while loops
+        int k = 100;
+       do {
+           System.out.println(k);
+           k = k - 1;
+        } while(k >= 1);
+
+
+            }
+        }
+
+
+
